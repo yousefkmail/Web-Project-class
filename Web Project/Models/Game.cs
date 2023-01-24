272c97634bcf8924285fa8bc4c066a5f45a6d1ef
yousefkmail <yousef.hnaty@gmail.com>
2023-01-24 09:04:50 +0200

    Add project files.

---
+++ Web Project/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿namespace Web_Project.Models
+{
+    public class Game
+    {
+
+
+        public string releaseDate { get; set; }
+        public string name { get; set; }
+        public string src { get; set; }
+
+        public string type { get; set; }
+        public Game( string name, string releaseDate, string src, string type)
+        {
+            this.releaseDate = releaseDate;
+            this.name = name;
+            this.src = src;
+            this.type = type;
+        }
+    }
+}
